31bbb5d1ef87d83be798ffe62d07bf8be669523f
HopeMashal <mashal.hope@gmail.com>
2022-10-31 11:02:41 +0200

    edit few files

--- fridge-food-task/src/main/java/fridgefoodtask/Core/WordFile.java
+++ fridge-food-task/src/main/java/fridgefoodtask/Core/WordFile.java
@@ -1,5 +1,21 @@
 package fridgefoodtask.Core;
 
+import java.util.List;
+
 public class WordFile {
-  
+  public static void WriteTitleWithParagraphs(String filePath, String header, String footer, String title,
+      List<String> paragraphs) {
+
+  }
+
+  public static void WriteRecipeFile(String filePath, String header, String footer, String title,
+      List<String> paragraphs, String pictureURL, List<String> tableValues) {
+
+  }
+
+  public static void WriteContestFile(String filePath, String header, String footer, String title,
+      List<String> paragraphs, String pictureURL) {
+    WriteTitleWithParagraphs(filePath, header, footer, title, paragraphs);
+
+  }
 }
