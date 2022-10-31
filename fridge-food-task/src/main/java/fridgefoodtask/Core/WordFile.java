57fc159942dfdbe0ac150a34688446b3e305909f
HopeMashal <mashal.hope@gmail.com>
2022-10-31 14:02:46 +0200

    edit word file

--- fridge-food-task/src/main/java/fridgefoodtask/Core/WordFile.java
+++ fridge-food-task/src/main/java/fridgefoodtask/Core/WordFile.java
@@ -3,7 +3,11 @@ package fridgefoodtask.Core;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.UnderlinePatterns;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 
 public class WordFile {
   XWPFDocument document;
@@ -15,16 +19,38 @@ public class WordFile {
     document = new XWPFDocument();
   }
 
-  public void AddTitle() {
-
+  public void AddTitle(String title) {
+    XWPFParagraph titleParagraph = document.createParagraph();
+    titleParagraph.setAlignment(ParagraphAlignment.CENTER);
+    XWPFRun titleRun = titleParagraph.createRun();
+    titleRun.setText(title);
+    titleRun.setColor("d51717");
+    titleRun.setBold(true);
+    titleRun.setFontFamily("New Roman");
+    titleRun.setFontSize(20);
   }
 
-  public void AddSubtitle() {
-
+  public void AddSubtitle(String subTitle) {
+    XWPFParagraph subTitleParagraph = document.createParagraph();
+    subTitleParagraph.setAlignment(ParagraphAlignment.LEFT);
+    XWPFRun subTitleRun = subTitleParagraph.createRun();
+    subTitleRun.setText(subTitle);
+    subTitleRun.setColor("ec4f4f");
+    subTitleRun.setFontFamily("Courier");
+    subTitleRun.setFontSize(16);
+    subTitleRun.setItalic(true);
+    subTitleRun.setTextPosition(20);
+    subTitleRun.setUnderline(UnderlinePatterns.DOT_DOT_DASH);
   }
 
-  public void AddParagraph() {
-
+  public void AddParagraph(String paragraph) {
+    XWPFParagraph paragraphParagraph = document.createParagraph();
+    paragraphParagraph.setAlignment(ParagraphAlignment.LEFT);
+    XWPFRun subTitleRun = paragraphParagraph.createRun();
+    subTitleRun.setText(paragraph);
+    subTitleRun.setColor("000000");
+    subTitleRun.setFontFamily("Calibri");
+    subTitleRun.setFontSize(14);
   }
 
   public void AddImage() {
@@ -54,4 +80,11 @@ public class WordFile {
     document.close();
   }
 
+  public static void main(String[] args) throws IOException {
+    WordFile wfile = new WordFile(Constant.getOutputFilesPath()+"test.docx");
+    wfile.AddTitle("Hello");
+    wfile.AddSubtitle("How are you!!");
+    wfile.AddParagraph("I'm OK!!!");
+    wfile.WriteWordFile();
+  }
 }
