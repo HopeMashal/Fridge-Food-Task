0ff34c63b1fb8103ff6bbe295197ef4b90ec7548
HopeMashal <mashal.hope@gmail.com>
2022-11-01 16:00:14 +0200

    edit search page

--- fridge-food-task/src/main/java/fridgefoodtask/Pages/SearchPage.java
+++ fridge-food-task/src/main/java/fridgefoodtask/Pages/SearchPage.java
@@ -1,8 +1,25 @@
 package fridgefoodtask.Pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SearchPage extends Navbar {
+  List<WebElement> firstPageResults;
+  List<WebElement> firstPageResultsLink;
+  List<WebElement> firstPageResultsImgSrc;
+  List<WebElement> firstPageResultsName;
+  List<WebElement> firstPageResultsCategory;
+  List<WebElement> firstPageResultsBookmarksBtn;
+
+  List<WebElement> homeSearchFirstPageResults;
+  List<WebElement> homeSearchFirstPageResultsLink;
+  List<WebElement> homeSearchFirstPageResultsImgSrc;
+  List<WebElement> homeSearchFirstPageResultsName;
+  List<WebElement> homeSearchFirstPageResultsCookingTime;
+  List<WebElement> homeSearchFirstPageResultsAddIngredientName;
+  List<WebElement> homeSearchFirstPageResultsBookmarksBtn;
 
   public SearchPage(WebDriver driver) {
     super(driver);
