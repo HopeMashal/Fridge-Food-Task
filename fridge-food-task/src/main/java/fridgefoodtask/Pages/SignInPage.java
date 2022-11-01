1d9bbb3e71329bb68061b9a97ed7913d2c9358a3
HopeMashal <mashal.hope@gmail.com>
2022-11-01 11:26:10 +0200

    sign in page DONE :)

--- fridge-food-task/src/main/java/fridgefoodtask/Pages/SignInPage.java
+++ fridge-food-task/src/main/java/fridgefoodtask/Pages/SignInPage.java
@@ -1,12 +1,30 @@
 package fridgefoodtask.Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SignInPage extends Navbar {
+  WebElement emailBox;
+  WebElement passwordBox;
+  WebElement loginBtn;
+  WebElement registerBtn;
 
   public SignInPage(WebDriver driver) {
     super(driver);
-    //TODO Auto-generated constructor stub
+    // TODO Auto-generated constructor stub
   }
 
+  public void signInMethod(String email, String password) {
+    emailBox = driver.findElement(By.id("txtEmail"));
+    emailBox.sendKeys(email);
+    passwordBox = driver.findElement(By.id("txtpassword"));
+    passwordBox.sendKeys(password);
+    loginBtn = driver.findElement(By.id("getbutton"));
+    loginBtn.click();
+  }
+
+  public void clickRegisterBtn() {
+    registerBtn = driver.findElement(By.id("registration"));
+  }
 }
