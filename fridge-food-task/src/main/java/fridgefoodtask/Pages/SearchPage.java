2146acf8381e0914c0ea5a2d600a8117e4cdaf03
HopeMashal <mashal.hope@gmail.com>
2022-11-01 19:48:54 +0200

    edit search page

--- fridge-food-task/src/main/java/fridgefoodtask/Pages/SearchPage.java
+++ fridge-food-task/src/main/java/fridgefoodtask/Pages/SearchPage.java
@@ -35,6 +35,29 @@ public class SearchPage extends Navbar {
   public List<String[]> getHomeSearchFirstPageResults() {
     List<String[]> results = new ArrayList<String[]>();
     homeSearchFirstPageResults = driver.findElements(By.className("recipe-tile recipe"));
+    for (WebElement result : firstPageResults) {
+      String resultLink = result.findElement(By.cssSelector("div[class='line-item-image-container']>a"))
+          .getAttribute("href");
+      String resultName = result.findElement(By.cssSelector("div[class='line-item-body']>a")).getText();
+      String resultCookingTime = result.findElement(By.cssSelector("div[class='line-item-details']")).getText();
+      String resultImgSrc = result.findElement(By.cssSelector("div[class='line-item-image-container']>a>img"))
+          .getAttribute("src");
+      String resultIngredients = result.findElement(By.cssSelector("div[class='missing-ingred-container']>span"))
+          .getText();
+      String resultIngredientsValue = "None";
+      if (resultIngredients.equals("Missing Ingredients")) {
+        resultIngredientsValue = "";
+        List<WebElement> IngredientsList = result
+            .findElements(By.cssSelector("div[class='flex-row']>span[class='AddIngredientName']"));
+        for (WebElement ingredient : IngredientsList) {
+          resultIngredientsValue += ingredient.getText() + ", ";
+        }
+        resultIngredientsValue = resultIngredientsValue.substring(0, resultIngredientsValue.length() - 2);
+      }
+      String[] resultValues = new String[] { resultName, resultLink, resultCookingTime, resultImgSrc,
+          resultIngredientsValue };
+      results.add(resultValues);
+    }
     return results;
   }
 
