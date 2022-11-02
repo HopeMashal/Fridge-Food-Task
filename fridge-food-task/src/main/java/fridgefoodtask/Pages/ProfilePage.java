febb15ff5d9c77d6bf7217f279039554ae84c6ca
HopeMashal <mashal.hope@gmail.com>
2022-11-02 13:20:27 +0200

    profile page DONE :)

--- fridge-food-task/src/main/java/fridgefoodtask/Pages/ProfilePage.java
+++ fridge-food-task/src/main/java/fridgefoodtask/Pages/ProfilePage.java
@@ -1,14 +1,49 @@
 package fridgefoodtask.Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import fridgefoodtask.Components.Navbar;
 
 public class ProfilePage extends Navbar {
+  WebElement firstNameBox;
+  WebElement lastNameBox;
+  WebElement countryBox;
+  WebElement stateBox;
+  WebElement cityBox;
 
   public ProfilePage(WebDriver driver) {
     super(driver);
     // TODO Auto-generated constructor stub
   }
 
+  public String getFirstNameValue() {
+    firstNameBox = driver.findElement(By.id("txtfname"));
+    return firstNameBox.getText();
+  }
+
+  public String getLastNameValue() {
+    lastNameBox = driver.findElement(By.id("txtlname"));
+    return lastNameBox.getText();
+  }
+
+  public String getCountryValue() {
+    countryBox = driver.findElement(By.id("ddcountry"));
+    return countryBox.getText();
+  }
+
+  public String getStateValue() {
+    if (!driver.findElements(By.id("ddstate")).isEmpty()) {
+      stateBox = driver.findElement(By.id("ddstate"));
+    } else {
+      stateBox = driver.findElement(By.id("txtstate"));
+    }
+    return stateBox.getText();
+  }
+
+  public String getCityValue() {
+    cityBox = driver.findElement(By.id("txtCity"));
+    return cityBox.getText();
+  }
 }
--- fridge-food-task/src/test/java/fridgefoodtask/PagesTest/RecipeTest.java
+++
@@ -1,7 +0,0 @@
-package fridgefoodtask.PagesTest;
-
-import fridgefoodtask.Core.TaskBase;
-
-public class RecipeTest extends TaskBase {
-  
-}
