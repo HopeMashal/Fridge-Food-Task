5222f90ffa975e847cfabbe02191e5d4fd7c9f97
HopeMashal <mashal.hope@gmail.com>
2022-11-02 14:38:54 +0200

    decider page DONE :)

--- fridge-food-task/src/main/java/fridgefoodtask/Pages/DeciderPage.java
+++ fridge-food-task/src/main/java/fridgefoodtask/Pages/DeciderPage.java
@@ -1,14 +1,33 @@
 package fridgefoodtask.Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import fridgefoodtask.Components.Navbar;
 
 public class DeciderPage extends Navbar {
+  WebElement answerBtn;
+  WebElement pickOneResultBtn;
+  WebElement startOverBtn;
 
   public DeciderPage(WebDriver driver) {
     super(driver);
     // TODO Auto-generated constructor stub
   }
 
+  public void clickAnswerBtn(String answer) {
+    answerBtn = driver.findElement(By.linkText(answer));
+    answerBtn.click();
+  }
+
+  public void clickPickOneResultBtn() {
+    pickOneResultBtn = driver.findElement(By.linkText("Just Pick One I Can Make!"));
+    pickOneResultBtn.click();
+  }
+
+  public void clickStartOverBtn() {
+    startOverBtn = driver.findElement(By.linkText("Start Over"));
+    startOverBtn.click();
+  }
 }
