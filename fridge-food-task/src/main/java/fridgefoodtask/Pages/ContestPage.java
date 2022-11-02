b79ae934a5848e0e1df86d9bb254d2a13dc7ee73
HopeMashal <mashal.hope@gmail.com>
2022-11-02 09:51:58 +0200

    contest page DONE :)

--- fridge-food-task/src/main/java/fridgefoodtask/Pages/ContestPage.java
+++ fridge-food-task/src/main/java/fridgefoodtask/Pages/ContestPage.java
@@ -1,15 +1,71 @@
 package fridgefoodtask.Pages;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import fridgefoodtask.Components.Navbar;
+import fridgefoodtask.Core.Constant;
+import fridgefoodtask.Core.DownloadFile;
 
 public class ContestPage extends Navbar {
+  WebElement contestTitle;
+  List<WebElement> contestParagraphs;
+  List<WebElement> contestList;
+  WebElement contestImg;
+  List<WebElement> archivedContest;
 
   public ContestPage(WebDriver driver) {
     super(driver);
-    //TODO Auto-generated constructor stub
+    // TODO Auto-generated constructor stub
   }
-  
-}
 
+  public String getContestTitle() {
+    contestTitle = driver.findElement(By.cssSelector("div[class='page-block']>h1"));
+    return contestTitle.getText();
+  }
+
+  public List<String> getContestParagraphs() {
+    contestParagraphs = driver.findElements(By.cssSelector("div[class='page-block']>p"));
+    List<String> paragraphsList = new ArrayList<String>();
+    for (WebElement paragraph : contestParagraphs) {
+      paragraphsList.add(paragraph.getText());
+    }
+    return paragraphsList;
+  }
+
+  public List<String> getContestList() {
+    contestList = driver.findElements(By.cssSelector("div[class='page-block']>ul>li"));
+    List<String> contestsList = new ArrayList<String>();
+    for (WebElement element : contestList) {
+      contestsList.add(element.getText());
+    }
+    return contestsList;
+  }
+
+  public String getContestImgPath() throws MalformedURLException, IOException {
+    contestImg = driver.findElement(By.cssSelector("div[class='page-block']>p>img"));
+    String ImgUrl = contestImg.getAttribute("src").replace(" ", "%20");
+    DownloadFile downloadFile = new DownloadFile();
+    File image = downloadFile.DownloadFileMethod(ImgUrl,
+        Constant.getDownloadsPath() + getContestTitle().replace(" ", "") + ".jpg");
+    return image.getPath();
+  }
+
+  public List<String[]> getArchivedContest() {
+    archivedContest = driver.findElements(By.cssSelector("div[class='archived-contests']>ul>li"));
+    List<String[]> archivedContestsList = new ArrayList<String[]>();
+    for (WebElement element : archivedContest) {
+      String archivedTitle = element.findElement(By.tagName("a")).getText();
+      String archivedDate = element.findElement(By.tagName("span")).getText();
+      archivedContestsList.add(new String[] { archivedTitle, archivedDate });
+    }
+    return archivedContestsList;
+  }
+}
--- fridge-food-task/src/test/java/fridgefoodtask/PagesTest/NavbarTest.java
+++ fridge-food-task/src/test/java/fridgefoodtask/PagesTest/NavbarTest.java
@@ -2,35 +2,14 @@ package fridgefoodtask.PagesTest;
 
 import java.io.IOException;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-import fridgefoodtask.Core.CSVFile;
-import fridgefoodtask.Core.Constant;
 import fridgefoodtask.Core.TaskBase;
-import fridgefoodtask.Core.WordFile;
-import fridgefoodtask.Pages.RecipePage;
-import fridgefoodtask.Pages.SearchPage;
 
 public class NavbarTest extends TaskBase {
   @Test
   public void BeforeSignIn() throws IOException {
     System.out.println("Navbar - Before Sign In");
-    driver.get("https://www.myfridgefood.com/Search?q=cake");
-    SearchPage searchPage = new SearchPage(driver);
-    CSVFile.writeDataLineByLine(Constant.getCSVFilesPath() + "test1.csv", searchPage.getFirstPageResults(),
-        new String[] { "Recipe Name", "Recipe Link", "Recipe Category", "Recipe Image Source" });
-    searchPage.clickSelectResult(0);
-    RecipePage recipePage = new RecipePage(driver);
-    CSVFile.writeDataLineByLine(Constant.getCSVFilesPath() + "test2.csv", recipePage.getRecipeInformation(),
-        new String[] { "Desc", "Desc Value" });
-    System.out.println(driver.findElement(By.cssSelector("div[class='recipe-text']>div")).getText());
-    System.out.println("*********************");
-    System.out.println(driver.findElement(By.xpath("//*[@id='recipe-page-block']/div[1]/div[2]/div[1]")).getText());
-    System.out.println("-------------------------");
-    WordFile wfile = new WordFile(Constant.getWordFilesPath() + "test.docx");
-    wfile.AddTable(recipePage.getRecipeTable());
-    wfile.WriteWordFile();
   }
 
   @Test
