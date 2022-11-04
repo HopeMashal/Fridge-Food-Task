36b2e0659aa30011d6bba040bc5b198f983bc7da
HopeMashal <mashal.hope@gmail.com>
2022-11-04 09:13:01 +0200

    edit copyright and submit recipe test

--- fridge-food-task/src/test/java/fridgefoodtask/PagesTest/CopyrightPolicyTest.java
+++ fridge-food-task/src/test/java/fridgefoodtask/PagesTest/CopyrightPolicyTest.java
@@ -1,7 +1,59 @@
 package fridgefoodtask.PagesTest;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import fridgefoodtask.Core.Constant;
 import fridgefoodtask.Core.TaskBase;
+import fridgefoodtask.Core.WordFile;
+import fridgefoodtask.Pages.CopyrightPolicyPage;
+import io.qameta.allure.Allure;
 
 public class CopyrightPolicyTest extends TaskBase {
-  
+  @Test
+  public void CopyrightPolicyTesting() throws IOException {
+    Allure.step("Go to Copyright Policy Page Using Navbar Method");
+    CopyrightPolicyPage copyrightPolicyPage = new CopyrightPolicyPage(driver);
+    copyrightPolicyPage.clickCopyrightPolicyBtn();
+
+    Allure.step("Check URL & Title of Copyright Policy Page");
+    String[] copyrightHref = copyrightPolicyPage.copyrightPolicyHref();
+    Assert.assertEquals(driver.getTitle(), copyrightHref[0], "Title of Copyright Policy Page NOT MATCH");
+    Assert.assertEquals(driver.getCurrentUrl(), copyrightHref[1], "URL of Copyright Policy Page NOT MATCH");
+
+    Allure.step("Scroll Down 350px to Show Copyright Policy Page");
+    JavaScript.executeScript("window.scrollTo(0,350)");
+
+    Allure.step("Copyright Policy Page - Take Screen Shot");
+    File copyrightPage = takeScreenShot
+        .takeScreenShot(Constant.getScreenShotsPath() + "CopyrightPolicyTest/" + "CopyrightPolicyPage.jpg");
+    allureAttached.addImage(copyrightPage);
+
+    Allure.step("Write Copyright Policy Page File");
+    String wordFilePath = Constant.getWordFilesPath() + "CopyrightPolicyPage.docx";
+    WordFile wordFile = new WordFile(wordFilePath);
+    wordFile.AddHeader(driver.getTitle());
+    wordFile.AddFooter(driver.getCurrentUrl());
+    wordFile.AddParagraph(copyrightPolicyPage.getFirstElement());
+    for (String el : copyrightPolicyPage.getSecondElement()) {
+      wordFile.AddParagraph(el);
+    }
+    wordFile.AddLists(copyrightPolicyPage.getThirdElement());
+    for (String el : copyrightPolicyPage.getFourthElement()) {
+      wordFile.AddParagraph(el);
+    }
+    wordFile.AddLists(copyrightPolicyPage.getFifthElement());
+    for (String el : copyrightPolicyPage.getSixthElement()) {
+      wordFile.AddParagraph(el);
+    }
+    wordFile.AddLists(copyrightPolicyPage.getSeventhElement());
+    wordFile.WriteWordFile();
+
+    Allure.step("Attach Copyright Policy Page File");
+    File contestFile = new File(wordFilePath);
+    allureAttached.addFile(contestFile, "doc");
+  }
 }
--- fridge-food-task/src/test/java/fridgefoodtask/PagesTest/SubmitRecipeTest.java
+++ fridge-food-task/src/test/java/fridgefoodtask/PagesTest/SubmitRecipeTest.java
@@ -1,7 +1,48 @@
 package fridgefoodtask.PagesTest;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import fridgefoodtask.Core.Constant;
 import fridgefoodtask.Core.TaskBase;
+import fridgefoodtask.Core.WordFile;
+import fridgefoodtask.Pages.SubmitRecipePage;
+import io.qameta.allure.Allure;
 
 public class SubmitRecipeTest extends TaskBase {
-  
+  @Test
+  public void SubmitRecipeTesting() throws IOException {
+    Allure.step("Go to Submit a Recipe Page Using Navbar Method");
+    SubmitRecipePage submitRecipePage = new SubmitRecipePage(driver);
+    submitRecipePage.clickSubmitRecipeBtn();
+
+    Allure.step("Check URL & Title of Submit a Recipe Page");
+    String[] submitRecipeHref = submitRecipePage.submitRecipeHref();
+    Assert.assertEquals(driver.getTitle(), submitRecipeHref[0], "Title of Submit a Recipe Page NOT MATCH");
+    Assert.assertEquals(driver.getCurrentUrl(), submitRecipeHref[1], "URL of Submit a Recipe Page NOT MATCH");
+
+    Allure.step("Scroll Down 350px to Show Submit a Recipe Page");
+    JavaScript.executeScript("window.scrollTo(0,350)");
+
+    Allure.step("Submit a Recipe Page - Take Screen Shot");
+    File submitPage = takeScreenShot
+        .takeScreenShot(Constant.getScreenShotsPath() + "SubmitRecipeTest/" + "SubmitRecipePage.jpg");
+    allureAttached.addImage(submitPage);
+
+    Allure.step("Write Submit a Recipe Page File");
+    String wordFilePath = Constant.getWordFilesPath() + "SubmitRecipePage.docx";
+    WordFile wordFile = new WordFile(wordFilePath);
+    wordFile.AddHeader(driver.getTitle());
+    wordFile.AddFooter(driver.getCurrentUrl());
+    wordFile.AddTitle(submitRecipePage.getSubmitRecipeTitle());
+    wordFile.AddLists(submitRecipePage.getSubmitRecipeList());
+    wordFile.WriteWordFile();
+
+    Allure.step("Attach Submit a Recipe Page File");
+    File contestFile = new File(wordFilePath);
+    allureAttached.addFile(contestFile, "doc");
+  }
 }
